83739d1715f2c1b89ade777b2e38f593df05a3ae
Richard Rossberg <40209986+DerBeleidigende@users.noreply.github.com>
2021-10-20 21:24:10 +0200

    1.0.0
    1.0.0

---
+++ VM Creation Tool.ps1
@@ -0,0 +1,18 @@
+﻿$VMName = Read-Host "VM Name:"
+[int]$VMDisk = Read-Host "VM Fetsplattengröße: "
+[int]$VMMem = Read-Host "VM Ram (in GB):"
+[int]$VMCore = Read-Host "VM Kerne:"
+$VMGen = Read-Host "VM Generation: (1 oder 2 (UEFI): "
+$VMSwitch = Read-Host "Name des virtuellen Switch:"
+
+if (Test-Path "C:\VM\$VMName\Virtual Hard Disks\$VMName.vhdx" -PathType leaf)
+{
+
+}else {
+    New-VHD -Path "C:\VM\$VMName\Virtual Hard Disks\$VMName.vhdx" -Fixed -SizeBytes ($VMDisk*1024*1024*1024) -PhysicalSectorSizeBytes 4096
+}
+
+
+
+New-VM -Name $VMName -MemoryStartupBytes ($VMMem*1024*1024*1024) -BootDevice VHD -VHDPath "C:\VM\$VMName\Virtual Hard Disks\$VMName.vhdx" -Path "C:\VM\" -Generation $VMGen -Switch $VMSwitch
+Set-VM -Name $VMName -AutomaticStartAction "StartIfRunning" -AutomaticStopAction "Shutdown" -ProcessorCount $VMCore
