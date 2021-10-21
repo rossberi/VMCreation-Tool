374a61c773786b92958b3320b3ce31a7151f7e3d
Richard Rossberg <40209986+DerBeleidigende@users.noreply.github.com>
2021-10-21 09:43:43 +0200

    1.0.1
    IF VM exists

--- VM Creation Tool.ps1
+++ VM Creation Tool.ps1
@@ -5,14 +5,19 @@
 $VMGen = Read-Host "VM Generation: (1 oder 2 (UEFI): "
 $VMSwitch = Read-Host "Name des virtuellen Switch:"
 
-if (Test-Path "C:\VM\$VMName\Virtual Hard Disks\$VMName.vhdx" -PathType leaf)
-{
-
-}else {
-    New-VHD -Path "C:\VM\$VMName\Virtual Hard Disks\$VMName.vhdx" -Fixed -SizeBytes ($VMDisk*1024*1024*1024) -PhysicalSectorSizeBytes 4096
+$VMExists = get-vm -name $VMName -ErrorAction SilentlyContinue
+If ($VMExists){
+	Write "Diese VM existiert schon"
 }
+Else {
+	if (Test-Path "C:\VM\$VMName\Virtual Hard Disks\$VMName.vhdx" -PathType leaf)
+    {
 
+    }else {
+        New-VHD -Path "C:\VM\$VMName\Virtual Hard Disks\$VMName.vhdx" -Fixed -SizeBytes ($VMDisk*1024*1024*1024) -PhysicalSectorSizeBytes 4096
+    }
 
+        New-VM -Name $VMName -MemoryStartupBytes ($VMMem*1024*1024*1024) -BootDevice VHD -VHDPath "C:\VM\$VMName\Virtual Hard Disks\$VMName.vhdx" -Path "C:\VM\" -Generation $VMGen -Switch $VMSwitch
+        Set-VM -Name $VMName -AutomaticStartAction "StartIfRunning" -AutomaticStopAction "Shutdown" -ProcessorCount $VMCore
 
-New-VM -Name $VMName -MemoryStartupBytes ($VMMem*1024*1024*1024) -BootDevice VHD -VHDPath "C:\VM\$VMName\Virtual Hard Disks\$VMName.vhdx" -Path "C:\VM\" -Generation $VMGen -Switch $VMSwitch
-Set-VM -Name $VMName -AutomaticStartAction "StartIfRunning" -AutomaticStopAction "Shutdown" -ProcessorCount $VMCore
+}
